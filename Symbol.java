a1dd9754f680d9890ca8f9547b790fc97dd05f71
M. Arkan Haris <m.arkanharistkj@gmail.com>
2023-11-14 13:56:20 +0700

    membuat abstract class Symbol dengan abstract method getVictoryWords()

---
+++ Symbol.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class Symbol here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+abstract class Symbol
+{
+    abstract String getVictoryWords();
+}
