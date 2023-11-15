96e671d53b8ff676c606484be9999bbc813d4701
irfnriza <irfnriza@gmail.com>
2023-11-15 12:51:46 +0700

    mengisi kelas gunting, batu dan kertas

--- Batu.java
+++ Batu.java
@@ -5,29 +5,17 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Batu
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Batu
-     */
-    public Batu()
-    {
-        // initialise instance variables
-        x = 0;
+// Rock.java
+public class Batu extends Symbol {
+    @Override
+    String getName() {
+        return "Batu";
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    @Override
+    String getVictoryWords() {
+        return "batu menghancurkan gunting";
     }
 }
+
+
--- Gunting.java
+++ Gunting.java
@@ -5,29 +5,16 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Gunting
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Gunting
-     */
-    public Gunting()
-    {
-        // initialise instance variables
-        x = 0;
+// Paper.java
+public class Gunting extends Symbol {
+    @Override
+    String getName() {
+        return "Gunting";
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    @Override
+    String getVictoryWords() {
+        return "Gunting Memotong kertas";
     }
 }
+
