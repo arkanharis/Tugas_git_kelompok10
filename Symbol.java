8ec80b25d8aeeb610846a915250270a1b9cbbaea
M. Arkan Haris <m.arkanharistkj@gmail.com>
2023-11-15 21:10:24 +0700

    membuat class symbol dan method constructor

--- Symbol.java
+++ Symbol.java
@@ -5,8 +5,18 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-abstract class Symbol
-{
-    abstract String getName();
-    abstract String getVictoryWords();
+
+public class Symbol {
+    private String name;
+    private String[] symbolNames = {"Rock", "Paper", "Scissors"};
+
+
+    public Symbol(int choice) {
+        if (choice >= 1 && choice <= 3) {
+            this.name = symbolNames[choice - 1];
+        } else {
+            throw new IllegalArgumentException("Invalid choice for Symbol constructor: " + choice);
+        }
+    }
+
 }
